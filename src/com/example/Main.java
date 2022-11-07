1436c1e54acae899d06da8a76ec5dca0c35a6d80
piero091100 <piero9965@gmail.com>
2022-11-07 09:57:32 -0500

    second commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        byte var1 = 2;
+        System.out.println(var1);
+        short var2 = 15;
+        System.out.println(var2);
+        int var3 = 99;
+        System.out.println(var3);
+        long var4 = 999999999;
+        System.out.println(var4);
+        float var5 = 1.0f;
+        System.out.println(var5);
+        double var6 = 2.0d;
+        System.out.println(var6);
+        boolean var7 = false;
+        System.out.println(var7);
+        char var8 = 'p';
+        System.out.println(var8);
+    }
+}
